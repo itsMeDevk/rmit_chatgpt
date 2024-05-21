3b0967dad36ec369ed38640a0693e30710c5b88f
itsMeDevk <41274270+itsMeDevk@users.noreply.github.com>
2024-05-21 19:10:01 +1000

    Update config.py

--- Backend/src/config.py
+++ Backend/src/config.py
@@ -1,20 +1,20 @@
-USER_POOL_ID = 'ap-southeast-2_zJYvF1Ay8'
-CLIENT_ID = '5goslip1l9hm4rfd53ua8h3dis'
-AWS_REGION = 'ap-southeast-2'
-CLIENT_SECRET = 'r0cbjv028l24ugcq1bhv72ujdprh2dvad7lccnoualjk5ldkeji'
+USER_POOL_ID = ''
+CLIENT_ID = ''
+AWS_REGION = ''
+CLIENT_SECRET = ''
 PASSWORD_RESET_REQUIRED = 'PASSWORD_RESET_REQUIRED'
 DRIVER = 'redshift+redshift_connector'
-HOST = 'ad-scraping.308731136394.ap-southeast-2.redshift-serverless.amazonaws.com'
+HOST = ''
 RS_PORT = '5439'
-DATABASE = 'dev'
-USERNAME = 'admin'
-PWD = '#Rmit_ai_team2'
+DATABASE = ''
+USERNAME = ''
+PWD = ''
 
-AWS_ACCESS_KEY_ID = "ASIAUPYOPJGFAUALNRH2"
-AWS_SECRET_ACCESS_KEY = "WdXzZPbKOf+m9qwWh8oBS3fCG7UGvc/uI3mVSf+G"
-AWS_PROFILE_NAME = '308731136394_stem-research-Admin'
+AWS_ACCESS_KEY_ID = ""
+AWS_SECRET_ACCESS_KEY = ""
+AWS_PROFILE_NAME = ''
 
-HOST_PG = 'rmit-chatgpt.cf761x2msgm4.ap-southeast-2.rds.amazonaws.com'
+HOST_PG = ''
 PG_PORT = '5432'
 DATABASE_PG = 'rmit_chatgpt_db'
 USERNAME_PG = 'postgres'
